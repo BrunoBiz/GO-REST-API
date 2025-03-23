5bb0bec543372f7628eb18b6721bb80b0604cb56
BrunoBiz <brunobizdc@gmail.com>
2025-03-23 15:18:04 -0300

    Users, Login, Validation

---
+++ routes/users.go
@@ -0,0 +1,47 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request"})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		fmt.Println(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not parse request"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "user created successfuly"})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login Successful"})
+}
